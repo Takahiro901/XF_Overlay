5105faffa820d199e35f307671960401b0be3864
Takahiro901 <30286581+Takahiro901@users.noreply.github.com>
2019-01-24 23:04:51 +0900

    first-commit

---
+++ XF_Overlay/LoadingPopupPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Rg.Plugins.Popup.Pages;
+
+namespace XF_Overlay
+{
+    public partial class LoadingPopupPage : PopupPage
+    {
+        public LoadingPopupPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XF_Overlay/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
+
+namespace XF_Overlay
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //Rg.Plugins.Popupを使わない場合
+        async void OnOverlay(object sender, EventArgs e)
+        {
+            bglayer.IsVisible = true;
+            frame.IsVisible = true;
+            await Task.Delay(2000);
+            bglayer.IsVisible = false;
+            frame.IsVisible = false;
+        }
+
+        //Rg.Plugins.Popupを使う場合
+        async void Rg(object sender, EventArgs e)
+        {
+            var page = new LoadingPopupPage();
+            await PopupNavigation.Instance.PushAsync(page);
+            await Task.Delay(2000);
+            await PopupNavigation.Instance.PopAsync();
+        }
+
+    }
+}
